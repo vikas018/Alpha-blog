a3e96595cd9b53287fc1656f108d219485ef06c0
vikas yadav <VikasY@vueops.com>
2020-07-31 18:19:43 +0530

    Initial commit and add root route

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+    def show
+       @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.user = User.first
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            # redirect_to action: "show", id: @article
+            # redirect_to@article = Article.find(params[:id])
+            # redirect_to @article
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to action: "index"
+    end
+end
